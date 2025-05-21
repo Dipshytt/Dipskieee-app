afe1d4b342e4fd8121cb11eba3f7b2e9a713d474
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-21 15:13:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ st.write( "Jika ingin keinginan terwujud, perbanyaklah bersujud 🤘😝🤘")
 st.image("IMG_20250313_194334_316.jpg", width=2000)
 
 angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
-st.write(angka, "jackpot" or "Zonk")
+st.write(angka, "jackpot"/"Zonk")
