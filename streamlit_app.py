ba5c8696f7b497f0ea1bae26c6be5b2bdb730c7f
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-21 15:12:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,4 @@ st.write( "Jika ingin keinginan terwujud, perbanyaklah bersujud 🤘😝🤘")
 st.image("IMG_20250313_194334_316.jpg", width=2000)
 
 angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
-
-
-if (angka % 2) == 0:
-  st.write(f"{angka} adalah Bilangan Genap")
-else:
-  st.write(f"{angka} adalah Bilangan Ganjil")
-
-st.title("tes")
-
-st.text_input("Masukan Nama:", value=0)
+print(angka, "jackpot" or "Zonk")
