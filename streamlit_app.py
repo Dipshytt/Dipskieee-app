a4cf98c4f77e9ac5435e8603982fe815562a1ba3
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-21 14:45:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,4 @@ import streamlit as st
 st.title("Nadhif Safaraz Pranggana")
 st.write( "Jika ingin keinginan terwujud, perbanyaklah bersujud 🤘😝🤘")
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.header("Kucing bageur")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-st.fileuploader
+st.image("images (56).jpeg", width=200)
