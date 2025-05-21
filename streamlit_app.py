9ae9f2c7086246108460840b969720cdb93fc75a
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-21 15:19:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,9 @@ if (angka % 2) ==0:
   st.write(f"{angka} adalah Bilangan Genap")
 else:
   st.write(f"{angka} adalah Bilangan Ganjil")
+
+nomor = st.number_input("Pilih sebuah angka:", value=0, step=1)
+if (nomor ≠ 270) ==0:
+  st.write(f"{nomor} Jackpot🤩🤩🤩")
+else:
+  st.write(f"{nomor} Zonk😭😭😭")
