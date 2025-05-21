1aeb827cf3d8b8f3bdceabe705a973968bf7bd3e
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-21 14:49:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("Nadhif Safaraz Pranggana")
 st.write( "Jika ingin keinginan terwujud, perbanyaklah bersujud 🤘😝🤘")
 
 st.image("images (56).jpeg", width=200)
+st.imagr("IMG_20250313_194334_316.jpg", width=2000)
