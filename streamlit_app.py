e8fa7261ec5b9888dc7692910b266f3c20f087a1
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-21 14:55:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,12 @@ import streamlit as st
 st.title("Nadhif Safaraz Pranggana")
 st.write( "Jika ingin keinginan terwujud, perbanyaklah bersujud 🤘😝🤘")
 
-st.image("images (56).jpeg", width=200)
 st.image("IMG_20250313_194334_316.jpg", width=2000)
+
+angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
+
+
+if (angka % 2) == 0:
+  st.write(f:"{angka} adalah Bilangan Genap")
+else:
+  st.write(f:"{angka} adalah Bilangan Ganjil")
