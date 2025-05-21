9819ed45a495d9fa8b32c01face4052655c4f008
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-21 14:55:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
 
 
 if (angka % 2) == 0:
-  st.write(f:"{angka} adalah Bilangan Genap")
+  st.write(f"{angka} adalah Bilangan Genap")
 else:
-  st.write(f:"{angka} adalah Bilangan Ganjil")
+  st.write(f"{angka} adalah Bilangan Ganjil")
