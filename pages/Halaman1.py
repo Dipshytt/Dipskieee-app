6741f55ae47568839ce43af8f6610bf6ea15eca0
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-22 21:13:48 +0700

    Update Halaman1.py

--- pages/Halaman1.py
+++ pages/Halaman1.py
@@ -1,1 +1,6 @@
+import streamlit as st
+
+st.title("Halaman Baru")
+st.subheader("Ini adalah halaman baru yang ditambahkan.")
+st.write("Konten halaman ini bisa berupa teks, grafik, atau interaksi lainnya.")
 
