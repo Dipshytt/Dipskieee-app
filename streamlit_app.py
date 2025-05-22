814ff2e17ae817dac9c6620b23ef564a9f4376f9
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-22 18:33:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Nadhif Safaraz Pranggana")
 st.write( "Jika ingin keinginan terwujud, perbanyaklah bersujud 🤘😝🤘")
-
+st.markdown("[Coba Klik](https://youtu.be/2Iyj3CBsxTk)")
 st.image("IMG_20250421_152717_503.jpg", width=2000)
 st.markdown("---")
 st.header("Aplikasi Ganjil Genap")
