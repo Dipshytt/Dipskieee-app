357a3947c528316806ad44655edc65b02e418893
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-22 21:16:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,52 @@ elif menu == "Halaman 2":
     st.title("Halaman 2")
     st.write("Ini konten halaman kedua.")
 
+
+
+
+st.write( "Jika ingin keinginan terwujud, perbanyaklah bersujud 🤘😝🤘")
+st.write("[Coba Klik](https://youtu.be/2Iyj3CBsxTk)")
+st.image("IMG_20250421_152717_503.jpg", width=2000)
+st.write("ayang aku 🥰🥰")
+st.write("---")
+st.header("Aplikasi Ganjil Genap")
+          
+angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
+if (angka % 2) ==0:
+  st.write(f"{angka} adalah Bilangan Genap")
+else:
+  st.write(f"{angka} adalah Bilangan Ganjil")
+
+st.write("---")
+
+st.header("To Do List")
+mantap = st.checkbox("Ngerjain tugas")
+mantap = st.checkbox("ngedate sama ayang")
+mantap = st.checkbox("menyelamatkan dunia")
+mantap = st.checkbox("menjadi presiden Indonesia")
+
+if mantap:
+          st.write("mantap")
+else:
+  pass
+
+st.write("---")
+st.header("gacha")
+
+nomor = st.number_input("Pilih angka random", value=0, step=1)
+
+sigma = ("selamat!!")
+skibidi = ("ayo coba lagi")
+slebew = ("Ayo Mulai")
+
+berhasil = (nomor == 261008)
+idle = (nomor == 0)
+gagal = (nomor != 261008)
+gagal = (nomor !=0)
+
+if berhasil:
+          st.write(sigma)
+if gagal:
+          st.write(skibidi)
+if idle:
+          st.write(slebew)
