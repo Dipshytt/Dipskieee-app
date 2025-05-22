2a403e07aeb8e5b3d4a9672152e4bb0550757658
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-22 21:53:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ if menu == "Home":
     st.write("[Coba Klik](https://youtu.be/2Iyj3CBsxTk)")
     st.image("IMG_20250421_152717_503.jpg", width=2000)
     ayang = st.text_input("nama orang diatas siapa 🫵😠")
-    if ayang == "eunchae" and ayang == "Eunchae":
+    if ayang == "Eunchae":
                           st.write("bener 🥰")
     else:
                           st.write("salah 😡")
