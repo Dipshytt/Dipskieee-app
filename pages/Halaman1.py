06e95861cf3c8e9aceb9c29a9e872bcb595514b2
Dipshytt <nadhifsafarazpranggana@gmail.com>
2025-05-22 21:15:44 +0700

    Delete pages directory

--- pages/Halaman1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("Halaman Baru")
-st.subheader("Ini adalah halaman baru yang ditambahkan.")
-st.write("Konten halaman ini bisa berupa teks, grafik, atau interaksi lainnya.")
-
